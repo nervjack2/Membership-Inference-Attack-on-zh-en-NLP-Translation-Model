68ab0792f3998f7409f33f303f2a6374e0c7da15
nervjack2 <nervjack2@gmail.com>
2021-12-29 23:26:05 +0800

    update download_data script

---
+++ translation/download_dataset.sh
@@ -0,0 +1,3 @@
+wget https://www.dropbox.com/s/umbm7fi5hgythag/splited_data_v2.zip?dl=1 -O data.zip 
+unzip data.zip 
+rm data.zip
--- translation/download_model.sh
+++ translation/download_model.sh
@@ -1,4 +1,3 @@
-wget https://www.dropbox.com/s/facicd2nuqjbnc9/splited_data.zip?dl=1 -O data.zip
 wget https://www.dropbox.com/sh/fb6itf9li0ny1x2/AAASMgks5FwyA90RqclmMKmaa?dl=1 -O Alice_target.zip
 mkdir Alice_target 
 unzip Alice_target.zip -d Alice_target/
